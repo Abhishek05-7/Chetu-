484fde96c369e086592b143e17b6406e5633d95a
Abhishek Singh <79144643+Abhishek05-7@users.noreply.github.com>
2022-04-29 19:57:47 +0530

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter A String");
+            StringBuilder str = new StringBuilder();
+            str.Append("Abhishek");
+            str.Append("\tLoves\t");
+            str.Append("Himself\t");
+
+            Console.WriteLine(str);
+
+            Console.ReadLine();
+        }
+    }
+}
