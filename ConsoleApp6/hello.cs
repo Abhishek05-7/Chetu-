cfa2fd8d52ca45cf5fbf238a9c1728c37059b815
Abhishek Singh <79144643+Abhishek05-7@users.noreply.github.com>
2022-05-13 18:54:43 +0530

    Add files via upload

---
+++ ConsoleApp6/hello.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleApp5;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class hello : Program
+
+    {
+        static void Main(string[] args)
+        {
+            M1();
+       
+            M4();
+            M5();
+
+        }
+    }
+}
