8351fc6832da318f3e021c0a7670c6d4a3c33be0
Abhishek Singh <79144643+Abhishek05-7@users.noreply.github.com>
2022-05-13 18:53:48 +0530

    Add files via upload

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Example
+    {
+        string name;
+        int age;
+
+        public Example(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public Example(Example example)
+        {
+            this.name = example.name;
+            this.age = example.age;
+        }
+        public void getShow()
+        {
+            Console.WriteLine("My name is:{0} ", name);
+            Console.WriteLine("My age is:{0} ", age);
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Example obj = new Example("amit", 23);
+            obj.getShow();
+            Example obj1 = new Example(obj);
+            obj1.getShow();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
